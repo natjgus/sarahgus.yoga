4f631b7ee4678ce55488a8f4fdc65ae753ba247d
Nathaniel Gustafson <natjgus@gmail.com>
2020-02-17 20:14:06 -0700

    jquery form submission

--- javascript/mailer.js
+++ javascript/mailer.js
@@ -1,24 +1,42 @@
-$(document).ready(function() {
-    // jQuery Validation
-    $("#signup").validate({
-        // if valid, post data via AJAX
-        submitHandler: function(form) {
-            $.post("./php/subscribe.php", { fname: $("#fname").val(), lname: $("#lname").val(), email: $("#email").val() }, function(data) {
-                $('#response').html(data);
-            });
-        },
-        // all fields are required
-        rules: {
-            fname: {
-                required: true
-            },
-            lname: {
-                required: true
-            },
-            email: {
-                required: true,
-                email: true
-            }
-        }
-    });
-});
+
+
+
+
+
+
+
+$("#contact-form").submit(function(e) {
+    e.preventDefault();
+  
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+        $("#formAlert").addClass('show');
+        setTimeout(function() {
+          $("#formAlert").removeClass('show')}, 2000);
+        });
+  });
+
+// $(document).ready(function() {
+//     // jQuery Validation
+//     $("#signup").validate({
+//         // if valid, post data via AJAX
+//         submitHandler: function(form) {
+//             $.post("./php/subscribe.php", { fname: $("#fname").val(), lname: $("#lname").val(), email: $("#email").val() }, function(data) {
+//                 $('#response').html(data);
+//             });
+//         },
+//         // all fields are required
+//         rules: {
+//             fname: {
+//                 required: true
+//             },
+//             lname: {
+//                 required: true
+//             },
+//             email: {
+//                 required: true,
+//                 email: true
+//             }
+//         }
+//     });
+// });
